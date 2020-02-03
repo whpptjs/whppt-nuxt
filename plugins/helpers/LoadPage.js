d70b06d083b543ba0bdca6db444f6078f6928f51
lucasjm <lvcasjm@gmail.com>
2020-02-03 21:45:48 +1030

    Working on API URL in config

--- plugins/helpers/LoadPage.js
+++ plugins/helpers/LoadPage.js
@@ -2,7 +2,7 @@ export default context => ({ slug }) => {
   const { $axios, store } = context;
   // const { $whppt, store } = context;
 
-  return $axios.get(`http://localhost:3001/api/page/load?slug=${slug}`).then(page => {
+  return $axios.get(`/api/page/load?slug=${slug}`).then(page => {
     // return $whppt.loadPage(slug).then(page => {
     return store.commit('whppt-nuxt/page/loaded', page);
   });
