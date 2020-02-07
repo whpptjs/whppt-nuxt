1f429880a10e4378591ffe200ea03870d487ef65
Ethan <sunnysun9312@gmail.com>
2020-02-07 16:52:44 +1030

    Update TextBox.vue

--- components/lib/system/TextBox.vue
+++ components/lib/system/TextBox.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="whppt-full ">
     <h1>Text box</h1>
-    <whppt-input-text
+    <whppt-text-input
       v-model="$whppt.editData[$whppt.editDataProperty]"
       class="whppt-textBox--margin-top-20"
       placeholder="Enter text here"
