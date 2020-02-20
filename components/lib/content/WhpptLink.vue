f92337b7ff31a9a78b5ce56f41a3b5430074e8f9
Jake Vandborg <jakevandborg@Jakes-MacBook-Pro.local>
2020-02-20 12:37:35 +1030

    removed outer div from whppt link

--- components/lib/content/WhpptLink.vue
+++ components/lib/content/WhpptLink.vue
@@ -3,16 +3,15 @@
     <div v-if="!isLinkActive">
       <slot></slot>
     </div>
-    <div v-else>
-      <component
-        :is="linkType"
-        :to="linkType === 'nuxt-link' && to.href"
-        :href="linkType === 'a' && to.href"
-        :target="linkType === 'a' && to.type === 'external' && '_blank'"
-      >
-        <slot></slot>
-      </component>
-    </div>
+    <component
+      :is="linkType"
+      v-else
+      :to="linkType === 'nuxt-link' && to.href"
+      :href="linkType === 'a' && to.href"
+      :target="linkType === 'a' && to.type === 'external' && '_blank'"
+    >
+      <slot></slot>
+    </component>
   </div>
 </template>
 
