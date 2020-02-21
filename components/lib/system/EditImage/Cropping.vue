210f6f45cb2e02019da6ca024e218a96402c5bff
Ethan <sunnysun9312@gmail.com>
2020-02-21 18:29:32 +1030

    Switching between components will change the image now

--- components/lib/system/EditImage/Cropping.vue
+++ components/lib/system/EditImage/Cropping.vue
@@ -6,19 +6,21 @@
         <label class="whppt-cropper-label">{{ canvas.name }}</label>
         <croppa
           :key="canvas.name"
-          v-model="canvas.croppa"
+          :ref="`${canvas.name}Croppa`"
           :disable-drag-and-drop="true"
           :disable-click-to-choose="true"
           :width="canvas.width / canvas.quality"
           :height="canvas.height / canvas.quality"
           :quality="canvas.quality"
           :initial-image="$whppt.originalImageUrl(imageOptionsCopy.imageId)"
-          :placeholder="'Choose an image'"
+          placeholder="Choose from gallery"
+          :placeholder-font-size="14"
           :accept="'image/*'"
           :file-size-limit="0"
           :prevent-white-space="true"
+          @new-image="test"
           @initial-image-loaded="applyManipulation"
-          @draw="change(canvas)"
+          @draw="change(canvas.name)"
         />
       </div>
       <whppt-button class="whppt-cropper__apply-change" @click="applyChanges">Apply Change</whppt-button>
@@ -27,11 +29,10 @@
 </template>
 
 <script>
-import { forEach, cloneDeep } from 'lodash';
+import { forEach, cloneDeep, map } from 'lodash';
 import Croppa from 'vue-croppa';
 import 'vue-croppa/dist/vue-croppa.css';
 import WhpptButton from '../../whpptComponents/WhpptButton';
-
 export default {
   name: 'EditorImageCropping',
   components: { croppa: Croppa.component, WhpptButton },
@@ -48,6 +49,7 @@ export default {
   data() {
     return {
       imageOptionsCopy: {},
+      croppas: [],
     };
   },
   computed: {
@@ -57,35 +59,58 @@ export default {
   },
   created() {
     // Reset crop seting for each image
-    this.imageOptions.crop = {};
+    this.imageOptions.crop = this.imageOptions.crop || {};
 
     forEach(this.sizes, (size, key) => {
+      // this.canvases.push({
+      //   name: key,
+      //   croppa: {},
+      //   ...size,
+      // });
       size.name = key;
-      size.croppa = size.croppa || {};
+      // size.croppa = size.croppa || {};
     });
 
     this.imageOptionsCopy = cloneDeep(this.imageOptions);
   },
   methods: {
+    test() {
+      console.log('test');
+    },
     applyManipulation() {
       this.$nextTick(() =>
         forEach(this.sizes, (size, key) => {
-          size.croppa.applyMetadata(this.imageOptions.crop[key] || {});
+          this.$refs[`${key}Croppa`][0].applyMetadata(this.imageOptions.crop[key] || {});
         })
       );
     },
-    change(canvas) {
+    change(sizeKey) {
+      const croppa = this.$refs[`${sizeKey}Croppa`][0];
       const meta = {
-        startX: canvas.croppa.imgData.startX,
-        startY: canvas.croppa.imgData.startY,
-        orientation: canvas.croppa.orientation,
-        scale: canvas.croppa.scaleRatio,
+        startX: croppa.imgData.startX,
+        startY: croppa.imgData.startY,
+        orientation: croppa.orientation,
+        scale: croppa.scaleRatio,
       };
 
-      this.imageOptionsCopy.crop[canvas.name] = meta;
+      this.imageOptionsCopy.crop[sizeKey] = meta;
+      console.log('TCL: change -> this.imageOptionsCopy', JSON.stringify(this.imageOptionsCopy, null, 2));
+      // this.imageOptionsCopy.imageId = canvas;
     },
     applyChanges() {
       this.imageOptions.crop = cloneDeep(this.imageOptionsCopy.crop);
+    },
+  },
+  watch: {
+    'imageOptions.imageId'(val) {
+      this.imageOptionsCopy.imageId = val;
+      const url = this.$whppt.originalImageUrl(this.imageOptionsCopy.imageId);
+      console.log('TCL: url', url);
+      forEach(this.sizes, (size, key) => {
+        size.name = key;
+        this.$refs[`${key}Croppa`][0].refresh();
+        this.$refs[`${key}Croppa`][0].applyMetadata(this.imageOptions.crop[key] || {});
+      });
     },
   },
 };
--- components/lib/system/EditImage/Gallery.vue
+++ components/lib/system/EditImage/Gallery.vue
@@ -1,28 +1,36 @@
 <template>
-  <div class="whppt-gallery-container">
-    <div class="whppt-gallery-item-container">
-      <div class="whppt-gallery__add" @click="$refs.fileInput.click()">
-        <input ref="fileInput" type="file" :accept="'image/*'" style="display: none;" @input="upload" />
-        <span>+</span>
-      </div>
-    </div>
-    <div v-for="image in images" :key="image._id" class="whppt-gallery-item-container">
-      <div
-        class="whppt-gallery-item"
-        :style="{ 'background-image': `url('${img(image._id)}')` }"
-        @click="$emit('input', image._id)"
-      >
-        <div class="whppt-gallery-item__remove" @click.stop="remove(image._id)">
-          <trash class="whppt-gallery-item__remove-icon" />
+  <div>
+    <loading v-if="loading" />
+    <div v-else class="whppt-gallery-container">
+      <div class="whppt-gallery-item-container">
+        <div class="whppt-gallery__add" @click="$refs.fileInput.click()" id="new-image-button">
+          <input ref="fileInput" type="file" :accept="'image/*'" style="display: none;" @input="upload" />
+          <span>+</span>
         </div>
       </div>
+      <div class="whppt-gallery-item-container" v-if="newImageLoading">
+        <div class="whppt-gallery__new">
+          <loading />
+        </div>
+      </div>
+      <div v-for="image in images" :key="image._id" class="whppt-gallery-item-container">
+        <div
+          class="whppt-gallery-item"
+          :style="{ 'background-image': `url('${img(image._id)}')` }"
+          @click="$emit('input', image._id)"
+        >
+          <div class="whppt-gallery-item__remove" @click.stop="remove(image._id)">
+            <trash class="whppt-gallery-item__remove-icon" />
+          </div>
+        </div>
+      </div>
+      <whppt-pagination
+        :current-page="currentPage"
+        :total="total"
+        :page-amount="Math.ceil(total / limit)"
+        @pageChanged="loadGallery"
+      />
     </div>
-    <whppt-pagination
-      :current-page="currentPage"
-      :total="total"
-      :page-amount="Math.ceil(total / limit)"
-      @pageChanged="loadGallery"
-    />
   </div>
 </template>
 
@@ -30,10 +38,11 @@
 import { mapState } from 'vuex';
 import icons from '../../icons';
 import WhpptPagination from '../../whpptComponents/WhpptPagination';
+import Loading from '../../icons/Loading';
 
 export default {
   name: 'EditorGallery',
-  components: { Trash: icons.Trash, WhpptPagination },
+  components: { Trash: icons.Trash, WhpptPagination, Loading },
   props: {
     value: {
       type: String,
@@ -44,10 +53,13 @@ export default {
     ...mapState('whppt-nuxt/editor', ['selectedComponent', 'baseImageUrl', 'baseAPIUrl']),
   },
   mounted() {
-    this.loadGallery(this.currentPage);
+    this.loading = true;
+    return this.loadGallery(this.currentPage).then(() => (this.loading = false));
   },
   data() {
     return {
+      loading: false,
+      newImageLoading: false,
       images: [],
       total: 0,
       limit: 9,
@@ -57,7 +69,7 @@ export default {
   methods: {
     loadGallery(currentPage) {
       this.currentPage = currentPage;
-      this.$axios
+      return this.$axios
         .get(`${this.baseAPIUrl}/api/image/loadGallery`, {
           params: { limit: this.limit, currentPage: this.currentPage },
         })
@@ -73,11 +85,17 @@ export default {
       const file = e.target.files[0];
       const formData = new FormData();
       formData.append('file', file);
-      return this.$axios.post(`${this.baseImageUrl}/upload`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
+      this.newImageLoading = true;
+      return this.$axios
+        .post(`${this.baseImageUrl}/upload`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then(() => this.loadGallery(1))
+        .then(() => {
+          this.newImageLoading = false;
+        });
     },
     remove(id) {
       return this.$axios
@@ -97,7 +115,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%;
+  height: 100px;
   border-color: white;
   border-width: 1px;
   border-style: dashed;
@@ -108,6 +126,14 @@ export default {
   background: white;
   color: black;
 }
+.whppt-gallery__new {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background: linear-gradient(45deg, #444 0%, #666 50%, #888 100%);
+}
+
 .whppt-gallery-item-container {
   flex-basis: 50%;
   padding: 5px;
@@ -119,13 +145,12 @@ export default {
   display: inline-flex;
 }
 .whppt-gallery-item {
-  background: tomato;
+  background: grey;
   height: 100px;
   cursor: pointer;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center center;
-  color: purple;
 }
 .whppt-gallery-item__remove {
   background: black;
