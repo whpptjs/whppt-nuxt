e4985acf7697dc6de3972a2efb164c404c485eaf
Matt Martin <matt.martin0108@gmail.com>
2020-04-27 15:58:13 +0930

    small and medium padding controls

--- components/lib/whpptComponents/SpacingControls.vue
+++ components/lib/whpptComponents/SpacingControls.vue
@@ -1,59 +1,156 @@
 <template>
-    <whppt-tabs>
-        <whppt-tab title="Large (Desktop)">
-            <div class="flex">
-                <whppt-text-input
-                    v-model="selectedComponent.value.marginTopLarge"
-                    type="number"
-                    max="8"
-                    min="0"
-                    :placeholder="$whppt.defaultMargin.top.base"
-                    label="Margin Top"
-                    class="marin-top-input w-5/12"
-                    @input="clampInput($event, 'marginTopLarge')"
-                />
-                <div class="w-1/12"></div>
-                <whppt-text-input
-                    v-model="selectedComponent.value.paddingTopLarge"
-                    type="number"
-                    max="8"
-                    min="0"
-                    :placeholder="$whppt.defaultPadding.top.base"
-                    label="Padding Top"
-                    class="marin-top-input w-5/12"
-                    @input="clampInput($event, 'paddingTopLarge')"
-                />
-                <div class="w-1/12"></div>
-            </div>
-            <div class="flex">
-                <whppt-text-input
-                    v-model="selectedComponent.value.marginBottomLarge"
-                    type="number"
-                    max="8"
-                    min="0"
-                    :placeholder="$whppt.defaultMargin.bottom.base"
-                    label="Margin Bottom"
-                    class="marin-top-input w-5/12"
-                    @input="clampInput($event, 'marginBottomLarge')"
-                />
-                <div class="w-1/12"></div>
-                <whppt-text-input
-                    v-model="selectedComponent.value.paddingBottomLarge"
-                    type="number"
-                    max="8"
-                    min="0"
-                    :placeholder="$whppt.defaultPadding.bottom.base"
-                    label="Padding Bottom"
-                    class="marin-top-input w-5/12"
-                    @input="clampInput($event, 'paddingBottomLarge')"
-                />
-                <div class="w-1/12"></div>
-            </div>
-        </whppt-tab>
-        <whppt-tab title="Medium (Tablet)"></whppt-tab>
-        <whppt-tab title="Small (Mobile)">
-        </whppt-tab>
-    </whppt-tabs>
+  <whppt-tabs>
+    <whppt-tab title="Large (Desktop)">
+      <div class="flex">
+        <whppt-text-input
+          v-model="selectedComponent.value.marginTopLarge"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultMargin.top.base"
+          label="Margin Top"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'marginTopLarge')"
+        />
+        <div class="w-1/12"></div>
+        <whppt-text-input
+          v-model="selectedComponent.value.paddingTopLarge"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultPadding.top.base"
+          label="Padding Top"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'paddingTopLarge')"
+        />
+        <div class="w-1/12"></div>
+      </div>
+      <div class="flex">
+        <whppt-text-input
+          v-model="selectedComponent.value.marginBottomLarge"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultMargin.bottom.base"
+          label="Margin Bottom"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'marginBottomLarge')"
+        />
+        <div class="w-1/12"></div>
+        <whppt-text-input
+          v-model="selectedComponent.value.paddingBottomLarge"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultPadding.bottom.base"
+          label="Padding Bottom"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'paddingBottomLarge')"
+        />
+        <div class="w-1/12"></div>
+      </div>
+    </whppt-tab>
+    <whppt-tab title="Medium (Tablet)">
+      <div class="flex">
+        <whppt-text-input
+          v-model="selectedComponent.value.marginTopMedium"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultMargin.top.base"
+          label="Margin Top"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'marginTopMedium')"
+        />
+        <div class="w-1/12"></div>
+        <whppt-text-input
+          v-model="selectedComponent.value.paddingTopMedium"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultPadding.top.base"
+          label="Padding Top"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'paddingTopMedium')"
+        />
+        <div class="w-1/12"></div>
+      </div>
+      <div class="flex">
+        <whppt-text-input
+          v-model="selectedComponent.value.marginBottomMedium"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultMargin.bottom.base"
+          label="Margin Bottom"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'marginBottomMedium')"
+        />
+        <div class="w-1/12"></div>
+        <whppt-text-input
+          v-model="selectedComponent.value.paddingBottomMedium"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultPadding.bottom.base"
+          label="Padding Bottom"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'paddingBottomMedium')"
+        />
+        <div class="w-1/12"></div>
+      </div>
+    </whppt-tab>
+    <whppt-tab title="Small (Mobile)">
+      <div class="flex">
+        <whppt-text-input
+          v-model="selectedComponent.value.marginTopSmall"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultMargin.top.base"
+          label="Margin Top"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'marginTopSmall')"
+        />
+        <div class="w-1/12"></div>
+        <whppt-text-input
+          v-model="selectedComponent.value.paddingTopSmall"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultPadding.top.base"
+          label="Padding Top"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'paddingTopSmall')"
+        />
+        <div class="w-1/12"></div>
+      </div>
+      <div class="flex">
+        <whppt-text-input
+          v-model="selectedComponent.value.marginBottomSmall"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultMargin.bottom.base"
+          label="Margin Bottom"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'marginBottomSmall')"
+        />
+        <div class="w-1/12"></div>
+        <whppt-text-input
+          v-model="selectedComponent.value.paddingBottomSmall"
+          type="number"
+          max="8"
+          min="0"
+          :placeholder="$whppt.defaultPadding.bottom.base"
+          label="Padding Bottom"
+          class="marin-top-input w-5/12"
+          @input="clampInput($event, 'paddingBottomSmall')"
+        />
+        <div class="w-1/12"></div>
+      </div>
+    </whppt-tab>
+  </whppt-tabs>
 </template>
 
 <script>
@@ -62,21 +159,21 @@ import WhpptTab from './WhpptTab';
 import WhpptTextInput from './WhpptTextInput';
 
 export default {
-    name: "SpacingControls",
-    props: {
-        selectedComponent: {
-            type: Object,
-            required: true
-        },
-        clampInput: {
-            type: Function,
-            required: true
-        }
+  name: 'SpacingControls',
+  components: {
+    WhpptTabs,
+    WhpptTab,
+    WhpptTextInput,
+  },
+  props: {
+    selectedComponent: {
+      type: Object,
+      required: true,
     },
-    components: {
-        WhpptTabs,
-        WhpptTab,
-        WhpptTextInput
-    }
-}
+    clampInput: {
+      type: Function,
+      required: true,
+    },
+  },
+};
 </script>
