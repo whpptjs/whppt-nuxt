c22d572089e3753c4444393ef89cd755fb4dbd76
Matt Martin <matt.martin0108@gmail.com>
2020-04-30 09:47:06 +0930

    contact icon improvements

--- components/lib/system/ContactIcon.vue
+++ components/lib/system/ContactIcon.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="whppt-full ">
-    <h1>Contact icon</h1>
+    <h2 class="text-xl">Contact icon</h2>
     <whppt-select
       v-model="selectedComponent.value.contactIcon"
       :value="selectedComponent.value.contactIcon"
-      label="Text Align"
+      label="Select an Icon"
       :items="items"
     ></whppt-select>
   </div>
