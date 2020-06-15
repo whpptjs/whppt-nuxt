beb7feef0b1015f81ec346cc4a3d8f29fe2566ea
Matt Martin <matt.martin0108@gmail.com>
2020-06-15 13:09:54 +0930

    keyprop for contact icon

--- components/lib/system/ContactIcon.vue
+++ components/lib/system/ContactIcon.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="whppt-full ">
+  <div class="whppt-full">
     <h2 class="text-xl">Contact icon</h2>
     <whppt-select
       v-model="selectedComponent.value.contactIcon"
       :value="selectedComponent.value.contactIcon"
       label="Select an Icon"
       :items="items"
+      key-prop="label"
     ></whppt-select>
   </div>
 </template>
