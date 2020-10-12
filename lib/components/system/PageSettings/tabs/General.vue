4bb44b7a04364f102d58fae141ff27c1c2d524c9
lucas <lvcasjm@gmail.com>
2020-10-12 12:14:16 +1030

    Prep for next rc

--- lib/components/system/PageSettings/tabs/General.vue
+++ lib/components/system/PageSettings/tabs/General.vue
@@ -1,120 +1,96 @@
 <template>
-  <div>
-    <div
-      v-if="showDupModal || showSlugModal || showWarning"
-      style="background: rgba(0, 0, 0, .5); position: absolute; top: 0; left: 0; right: 0; bottom: 0"
-    ></div>
-    <div v-if="showDupModal" class="whppt-settings__warning-modal">
-      <div class="whppt-settings__warning-content">
-        <form style="position: relative; " @submit.prevent>
-          <div class="whppt-settings__warning-heading" style="text-align: center;">Duplicate Page</div>
-          <div class="whppt-settings__warning-body">
-            <div>
-              <whppt-text-input
-                v-model="slugCopy"
-                placeholder="Enter a new slug for the duplicate"
-                label="Slug"
-                label-colour="black"
-                :info="
-                  `The page slug makes up part of the page's url that is shown in the browsers address bar and is used by search engines to match your page with search terms. \nYour input will be formatted according to the page type setting and to avoid certain characters.`
-                "
-              />
-              <div style="display: flex; align-items: center; justify-content: flex-start">
-                <div style="font-weight: bold; padding-right: 0.5rem;">Output:</div>
-                <div>
-                  {{ formattedSlug }}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="whppt-settings__warning-actions">
-            <whppt-button @click="saveDup">Save</whppt-button>
-            <whppt-button @click="showDupModal = false">Close</whppt-button>
-          </div>
-        </form>
-      </div>
-    </div>
-    <div v-if="showSlugModal" class="whppt-settings__warning-modal">
-      <div class="whppt-settings__warning-content">
-        <form style="position: relative; " @submit.prevent>
-          <div class="whppt-settings__warning-heading" style="text-align: center;">Edit Slug</div>
-          <div class="whppt-settings__warning-body">
-            <div>
-              <div v-if="prefix" class="whppt-flex-between whppt-align-center">
-                <whppt-text-input
-                  :value="prefix"
-                  :disabled="true"
-                  placeholder="Page Slug Prefix"
-                  label="Prefix"
-                  label-colour="black"
-                  info="This prefix is managed by Whppt and is not editable."
-                />
-                <whppt-text-input
-                  :value="slugSuffix"
-                  placeholder="Enter a page slug"
-                  label="Slug"
-                  label-colour="black"
-                  info="The page slug makes up part of the page's url that is shown in the browsers address bar and is used by search engines to match your page with search terms. Your input will be formatted to avoid certain characters."
-                  @input="confirmSlug"
-                />
-              </div>
-              <div v-if="!prefix">
-                <whppt-text-input
-                  v-model="slugCopy"
-                  placeholder="Enter a page slug"
-                  label="Slug"
-                  label-colour="black"
-                  :info="
-                    `The page slug makes up part of the page's url that is shown in the browsers address bar and is used by search engines to match your page with search terms. \nYour input will be formatted according to the page type setting and to avoid certain characters.`
-                  "
-                />
-              </div>
-              <div style="display: flex; align-items: center; justify-content: flex-start">
-                <div style="font-weight: bold; padding-right: 0.5rem;">Output:</div>
-                <div>
-                  {{ formattedSlug }}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="whppt-settings__warning-actions">
-            <button class="whppt-settings__button" style="margin-left: 0" @click="saveSlug">
-              Save
-            </button>
-            <button class="whppt-settings__button" @click="showSlugModal = false">Close</button>
-          </div>
-        </form>
-      </div>
-    </div>
-    <div v-if="showWarning" class="whppt-settings__warning-modal">
-      <div class="whppt-settings__warning-content">
-        <div style="position: relative; text-align: center;">
-          <div class="whppt-settings__warning-heading">Confirm Delete</div>
-          <div class="whppt-settings__warning-body">
-            <div>
-              Are you sure?
-            </div>
-            <div>
-              This will delete the page and all of its content.
-            </div>
-          </div>
-          <div class="whppt-settings__warning-actions">
-            <button
-              class="whppt-settings__button"
-              :disabled="isHomePage"
-              style="margin-left: 0"
-              @click="deletePageFromDraft()"
-            >
-              Delete
-            </button>
-            <button class="whppt-settings__button" @click="showWarning = false">Cancel</button>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="whppt-page-setting__general">
+    <!--    <div-->
+    <!--      v-if="showDupModal || showSlugModal || showWarning"-->
+    <!--      style="background: rgba(0, 0, 0, .5); position: absolute; top: 0; left: 0; right: 0; bottom: 0"-->
+    <!--    ></div>-->
+    <!--    <div v-if="showDupModal" class="whppt-settings__warning-modal">-->
+    <!--      <div class="whppt-settings__warning-content">-->
+    <!--        <form style="position: relative; " @submit.prevent>-->
+    <!--          <div class="whppt-settings__warning-heading" style="text-align: center;">Duplicate Page</div>-->
+    <!--          <div class="whppt-settings__warning-body">-->
+    <!--            <div>-->
+    <!--              <whppt-text-input-->
+    <!--                v-model="slugCopy"-->
+    <!--                placeholder="Enter a new slug for the duplicate"-->
+    <!--                label="Slug"-->
+    <!--                label-colour="black"-->
+    <!--                :info="-->
+    <!--                  `The page slug makes up part of the page's url that is shown in the browsers address bar and is used by search engines to match your page with search terms. \nYour input will be formatted according to the page type setting and to avoid certain characters.`-->
+    <!--                "-->
+    <!--              />-->
+    <!--              <div style="display: flex; align-items: center; justify-content: flex-start">-->
+    <!--                <div style="font-weight: bold; padding-right: 0.5rem;">Output:</div>-->
+    <!--                <div>-->
+    <!--                  {{ formattedSlug }}-->
+    <!--                </div>-->
+    <!--              </div>-->
+    <!--            </div>-->
+    <!--          </div>-->
+    <!--          <div class="whppt-settings__warning-actions">-->
+    <!--            <whppt-button @click="duplicatePage">Save</whppt-button>-->
+    <!--            <whppt-button @click="showDupModal = false">Close</whppt-button>-->
+    <!--          </div>-->
+    <!--        </form>-->
+    <!--      </div>-->
+    <!--    </div>-->
+    <!--    <div v-if="showSlugModal" class="whppt-settings__warning-modal">-->
+    <!--      <div class="whppt-settings__warning-content">-->
+    <!--        <form style="position: relative; " @submit.prevent>-->
+    <!--          <div class="whppt-settings__warning-heading" style="text-align: center;">Edit Slug</div>-->
+    <!--          <div class="whppt-settings__warning-body">-->
+    <!--            <div>-->
+    <!--              <div v-if="prefix" class="whppt-flex-between whppt-align-center">-->
+    <!--                <whppt-text-input-->
+    <!--                  :value="prefix"-->
+    <!--                  :disabled="true"-->
+    <!--                  placeholder="Page Slug Prefix"-->
+    <!--                  label="Prefix"-->
+    <!--                  label-colour="black"-->
+    <!--                  info="This prefix is managed by Whppt and is not editable."-->
+    <!--                />-->
+    <!--                <whppt-text-input-->
+    <!--                  :value="slugSuffix"-->
+    <!--                  placeholder="Enter a page slug"-->
+    <!--                  label="Slug"-->
+    <!--                  label-colour="black"-->
+    <!--                  info="The page slug makes up part of the page's url that is shown in the browsers address bar and is used by search engines to match your page with search terms. Your input will be formatted to avoid certain characters."-->
+    <!--                  @input="confirmSlug"-->
+    <!--                />-->
+    <!--              </div>-->
+    <!--              <div v-if="!prefix">-->
+    <!--                <whppt-text-input-->
+    <!--                  v-model="slugCopy"-->
+    <!--                  placeholder="Enter a page slug"-->
+    <!--                  label="Slug"-->
+    <!--                  label-colour="black"-->
+    <!--                  :info="-->
+    <!--                    `The page slug makes up part of the page's url that is shown in the browsers address bar and is used by search engines to match your page with search terms. \nYour input will be formatted according to the page type setting and to avoid certain characters.`-->
+    <!--                  "-->
+    <!--                />-->
+    <!--              </div>-->
+    <!--              <div style="display: flex; align-items: center; justify-content: flex-start">-->
+    <!--                <div style="font-weight: bold; padding-right: 0.5rem;">Output:</div>-->
+    <!--                <div>-->
+    <!--                  {{ formattedSlug }}-->
+    <!--                </div>-->
+    <!--              </div>-->
+    <!--            </div>-->
+    <!--          </div>-->
+    <!--          <div class="whppt-settings__warning-actions">-->
+    <!--            <button class="whppt-settings__button" style="margin-left: 0" @click="saveSlug">-->
+    <!--              Save-->
+    <!--            </button>-->
+    <!--            <button class="whppt-settings__button" @click="showSlugModal = false">Close</button>-->
+    <!--          </div>-->
+    <!--        </form>-->
+    <!--      </div>-->
+    <!--    </div>-->
     <whppt-card>
-      <span>Page Formatted Slug</span>
-      <div>{{ page.slug }}</div>
+      <span><strong>Page Formatted Slug</strong></span>
+      <div>
+        <span>current slug:</span> <strong>{{ `/${page.slug}` }}</strong>
+      </div>
       <whppt-button @click="openSlugModal">Change Slug</whppt-button>
     </whppt-card>
     <whppt-card>
@@ -138,11 +114,19 @@
       </form>
     </whppt-card>
     <whppt-card>
-      <div>
+      <div class="whppt-page__actions">
         <whppt-button @click="openDupModal">Duplicate Page</whppt-button>
         <whppt-button @click="showWarning = true">Delete Page</whppt-button>
       </div>
     </whppt-card>
+
+    <!-- Delete page dialog -->
+    <whppt-dialog :is-active="showWarning">
+      <whppt-card>
+        <whppt-button @click="deletePageFromDraft">Delete</whppt-button>
+        <whppt-button @click="showWarning = false">Cancel</whppt-button>
+      </whppt-card>
+    </whppt-dialog>
   </div>
 </template>
 
@@ -154,6 +138,7 @@ import WhpptCard from '../../../ui/Card';
 import WhpptSelect from '../../../ui/Select';
 import WhpptTextInput from '../../../ui/Input';
 import WhpptButton from '../../../ui/Button';
+import WhpptDialog from '../../../ui/Dialog';
 
 const additionalComponents = {};
 
@@ -172,6 +157,7 @@ export default {
     WhpptSelect,
     WhpptCard,
     WhpptButton,
+    WhpptDialog,
   },
   props: {
     page: { type: Object, required: true },
@@ -303,18 +289,22 @@ export default {
         }
       });
     },
-    saveDup() {
+    duplicatePage() {
       const vm = this;
+
       const newSlug = this.formattedSlug;
+
       if (!newSlug) {
         this.$toast.global.editorError('Cannot use an empty slug');
         return;
       }
+
       return vm.checkSlug({ slug: newSlug, pageType: vm.page.pageType }).then(result => {
         if (result) {
           this.$toast.global.editorError('Slug already in use');
         } else {
           vm.page.slug = newSlug;
+
           return vm.savePage({ page: { ...this.page, _id: undefined } }).then(() => {
             vm.$router.push(`/${newSlug}`);
             this.showDupModal = false;
@@ -325,11 +315,15 @@ export default {
     },
     deletePageFromDraft() {
       const vm = this;
+
       if (vm.page.published) {
-        return vm.$toast.global.editorError('Unpublish the page first');
+        return vm.$toast.global.editorError(
+          'This page is currently published, Please unpublish the page before deletion.'
+        );
       }
+
       return vm.deletePage().then(() => {
-        vm.$router.push(`/`);
+        !vm.page.slug || vm.page.slug === '/' ? vm.$router.push(`/404`) : vm.$router.push(`/`);
         vm.showWarning = false;
         vm.$emit('closeModal');
       });
@@ -339,19 +333,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.whppt-divider {
-  margin: 0.5rem 0;
-  width: 100%;
-  height: 1px;
-  background: lightgray;
-}
+.whppt-page-setting__general {
+  .whppt-divider {
+    margin: 0.5rem 0;
+    width: 100%;
+    height: 1px;
+    background: lightgray;
+  }
 
-.whppt-page__form fieldset {
-  margin: 1rem 0;
-}
+  .whppt-page__form fieldset {
+    margin: 1rem 0;
+  }
 
-.whppt-settings__section {
-  display: flex;
-  padding: 1rem;
+  .whppt-settings__section {
+    display: flex;
+    padding: 1rem;
+  }
+
+  .whppt-page__actions {
+    display: flex;
+
+    button {
+      margin-right: 0.5rem;
+    }
+  }
 }
 </style>
--- lib/components/system/PageSettings/tabs/SEO.vue
+++ lib/components/system/PageSettings/tabs/SEO.vue
@@ -1,93 +1,119 @@
 <template>
-  <form @submit.prevent>
-    <div>
-      <fieldset>
-        <div class="whppt-flex-between">
-          <div class="whppt-settings__left-column">
-            <whppt-text-input
-              v-model="page.title"
-              placeholder="Enter a page title"
-              label="Title"
-              label-colour="black"
-              info="The page title is shown in the browser's tab and used by search engines to match your page with search terms. Search results use the title to list the page."
-            />
-          </div>
-          <div class="whppt-settings__right-column">
-            <whppt-text-input
-              v-model="page.keywords"
-              placeholder="keywords (eg. page, simple)"
-              label="Keywords"
-              label-colour="black"
-              info="Keywords are not shown on the page and are used by search engines to match your page with search terms. Comma seperate your values to add multiple."
-            />
-          </div>
-        </div>
+  <div>
+    <whppt-card>
+      <form @submit.prevent>
         <div>
-          <whppt-text-area
-            v-model="page.description"
-            placeholder="Enter description"
-            label="Description"
-            rows="2"
-            label-colour="black"
-            info="The page description is not shown the page and is used by search engines to match your page with search terms. Search results can show this description."
-          />
-        </div>
-        <div class="whppt-flex-between">
-          <div class="whppt-settings__left-column">
-            <whppt-text-input
-              v-model="page.priority"
-              placeholder="Enter a priority"
-              label="Priority"
-              label-colour="black"
-              info="Priority lets search engines know which pages you deem most important. Values range from 0.0 to 1.0, with a default value of 0.5."
-              @change="clampInput"
-            />
-          </div>
-          <div class="whppt-settings__right-column">
-            <div class="whppt-select__frequency-label">Frequency</div>
-            <label>
-              <select v-model="page.frequency" class="whppt-select__frequency-input">
-                <option v-for="(item, index) in frequencies" :key="index">
-                  {{ item.value }}
-                </option>
-              </select>
-            </label>
-            <div class="whppt-select__frequency-info">
-              Frequency tells search engines how often the page is likely to change.
+          <fieldset>
+            <div class="whppt-flex-between">
+              <div class="whppt-settings__left-column">
+                <whppt-text-input
+                  v-model="pageSettings.title"
+                  placeholder="Enter a page title"
+                  label="Title"
+                  label-colour="black"
+                  info="The page title is shown in the browser's tab and used by search engines to match your page with search terms. Search results use the title to list the page."
+                />
+              </div>
+              <div class="whppt-settings__right-column">
+                <whppt-text-input
+                  v-model="pageSettings.keywords"
+                  placeholder="keywords (eg. page, simple)"
+                  label="Keywords"
+                  label-colour="black"
+                  info="Keywords are not shown on the page and are used by search engines to match your page with search terms. Comma separate your values to add multiple."
+                />
+              </div>
             </div>
-          </div>
+            <div>
+              <whppt-text-area
+                v-model="pageSettings.description"
+                placeholder="Enter description"
+                label="Description"
+                rows="2"
+                label-colour="black"
+                info="The page description is not shown the page and is used by search engines to match your page with search terms. Search results can show this description."
+              />
+            </div>
+            <div class="whppt-flex-between">
+              <div class="whppt-settings__left-column">
+                <whppt-text-input
+                  v-model="pageSettings.priority"
+                  placeholder="Enter a priority"
+                  label="Priority"
+                  label-colour="black"
+                  info="Priority lets search engines know which pages you deem most important. Values range from 0.0 to 1.0, with a default value of 0.5."
+                  @change="clampInput"
+                />
+              </div>
+              <div class="whppt-settings__right-column">
+                <div class="whppt-select__frequency-label">Frequency</div>
+                <whppt-select
+                  :id="`${$options._scopeId}-frequency`"
+                  v-model="pageSettings.frequency"
+                  :items="frequencies"
+                  item-text="id"
+                  item-value="value"
+                  info="Frequency tells search engines how often the page is likely to change."
+                />
+              </div>
+            </div>
+          </fieldset>
         </div>
-      </fieldset>
-    </div>
-  </form>
+      </form>
+    </whppt-card>
+    <whppt-card>
+      <whppt-button @click="saveSettings({ ...page, ...pageSettings })">Save Settings</whppt-button>
+    </whppt-card>
+  </div>
 </template>
 
 <script>
 import { clamp } from 'lodash';
 import WhpptTextInput from '../../../ui/Input';
 import WhpptTextArea from '../../../ui/Textarea';
-// import WhpptCard from '../../../ui/Card';
+import WhpptSelect from '../../../ui/Select';
+import WhpptButton from '../../../ui/Button';
+import WhpptCard from '../../../ui/Card';
+
+import savePageMixin from '../../../../util/mixins/pageSettings';
 
 export default {
   name: 'PageSettingsSEO',
   components: {
     WhpptTextInput,
     WhpptTextArea,
+    WhpptSelect,
+    WhpptButton,
+    WhpptCard,
   },
-  props: {
-    page: { type: Object, required: true },
-  },
+  mixins: [savePageMixin],
   data: () => ({
     frequencies: [
-      { value: 'never', id: 'never' },
-      { value: 'yearly', id: 'yearly' },
-      { value: 'monthly', id: 'monthly' },
-      { value: 'weekly', id: 'weekly' },
-      { value: 'daily', id: 'daily' },
-      { value: 'hourly', id: 'hourly' },
-      { value: 'always', id: 'always' },
+      { value: 'never', id: 'Never' },
+      { value: 'yearly', id: 'Yearly' },
+      { value: 'monthly', id: 'Monthly' },
+      { value: 'weekly', id: 'Weekly' },
+      { value: 'daily', id: 'Daily' },
+      { value: 'hourly', id: 'Hourly' },
+      { value: 'always', id: 'Always' },
     ],
+    pageSettings: {
+      title: '',
+      keywords: '',
+      description: '',
+      priority: '',
+      frequency: '',
+    },
   }),
+  created() {
+    if (this.page) {
+      this.pageSettings.title = this.page.title;
+      this.pageSettings.keywords = this.page.keywords;
+      this.pageSettings.description = this.page.description;
+      this.pageSettings.priority = this.page.priority;
+      this.pageSettings.frequency = this.page.frequency;
+    }
+  },
   methods: {
     clampInput(input) {
       return input ? (this.page.priority = clamp(input, 0, 1)) : 0.5;
