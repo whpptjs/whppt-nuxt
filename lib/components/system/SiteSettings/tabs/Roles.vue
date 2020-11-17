703a1878b758814bc97d883c23ccf40bb32b0245
lucas <lvcasjm@gmail.com>
2020-11-17 10:55:29 +1030

    Site setting roles progress

--- lib/components/system/SiteSettings/tabs/Roles.vue
+++ lib/components/system/SiteSettings/tabs/Roles.vue
@@ -30,11 +30,44 @@
         <template v-slot:item.updatedAt="{ item }">
           {{ formatDate(item.updatedAt) }}
         </template>
-        <template v-slot:item.actions>
-          <whppt-button flat @click="deleteRoleVisible">D</whppt-button>
+        <template v-slot:item.actions="{ item }">
+          <whppt-button flat @click="showDeleteRoleWarning(item)">
+            <svg
+              data-v-08a1318b=""
+              viewBox="0 0 20 20"
+              width="20"
+              height="20"
+              xmlns="http://www.w3.org/2000/svg"
+              data-v-0ae9d0f6=""
+            >
+              <path
+                d="M2 2h16v2H2V2zm6-2h4l2 2H6l2-2zM3 6h14l-1 14H4L3 6zm5 2h1v10H8V8zm3 0h1v10h-1V8z"
+                fill="none"
+                fill-rule="evenodd"
+                style="fill: currentcolor;"
+              ></path>
+            </svg>
+          </whppt-button>
         </template>
       </whppt-table>
     </whppt-card>
+    <whppt-dialog :is-active="deleteRoleVisible" :width="800" :height="300" @closed="deleteRoleVisible = false">
+      <template v-slot:header>
+        <whppt-toolbar>
+          <h3>Are you sure?</h3>
+        </whppt-toolbar>
+      </template>
+      <whppt-card class="whppt-dialog__warning">
+        <div class="whppt-dialog__warning-message">
+          <h4>Are you sure you want to delete this role?</h4>
+          <p><em>Warning: Once you delete this role there is no way to recover it.</em></p>
+        </div>
+        <div class="whppt-dialog__warning-actions">
+          <whppt-button danger @click="deleteRole">Delete</whppt-button>
+          <whppt-button flat @click="deleteRoleVisible = false">Cancel</whppt-button>
+        </div>
+      </whppt-card>
+    </whppt-dialog>
   </div>
 </template>
 
@@ -44,14 +77,18 @@ import WhpptCard from '../../../ui/Card';
 import WhpptTable from '../../../ui/Table';
 import WhpptInput from '../../../ui/Input';
 import WhpptButton from '../../../ui/Button';
+import WhpptToolbar from '../../../ui/Toolbar';
+import WhpptDialog from '../../../ui/Dialog';
 
 export default {
   name: 'SettingsRoles',
   components: {
+    WhpptToolbar,
     WhpptCard,
     WhpptTable,
     WhpptInput,
     WhpptButton,
+    WhpptDialog,
   },
   data: () => ({
     roles: [],
@@ -63,6 +100,7 @@ export default {
     },
     error: '',
     deleteRoleVisible: false,
+    roleToDelete: undefined,
   }),
   computed: {
     headers() {
@@ -103,6 +141,23 @@ export default {
     formatDate(date) {
       return dayjs(date).format('h:mma ddd D MMM YYYY');
     },
+    showDeleteRoleWarning(role) {
+      this.roleToDelete = role;
+      this.deleteRoleVisible = true;
+    },
+    deleteRole() {
+      this.$axios
+        .$post(`${this.$whppt.apiPrefix}/roles/delete`, { role: this.roleToDelete })
+        .then(() => {
+          this.roleToDelete = undefined;
+          this.deleteRoleVisible = false;
+        })
+        .catch(err => {
+          console.log(err);
+          this.roleToDelete = undefined;
+          this.deleteRoleVisible = false;
+        });
+    },
   },
 };
 </script>
@@ -112,6 +167,37 @@ export default {
   button {
     margin-top: 0.5rem;
     margin-left: auto;
+  }
+}
+
+.whppt-dialog__warning {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 0;
+  height: 100%;
+
+  h3,
+  h4 {
+    font-weight: bold;
+  }
+
+  p {
+    font-size: 0.75rem;
+  }
+}
+
+.whppt-dialog__warning-message {
+  margin-bottom: 1rem;
+}
+
+.whppt-dialog__warning-actions {
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  height: 100%;
+
+  button {
+    margin-left: 0.5rem;
   }
 }
 </style>
